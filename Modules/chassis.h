4cfdcaef74061e5930876d65b9faf7bf854d9c95
Wu Tianhao <didi636963@gmail.com>
2026-07-22 20:05:16 +0800

    merge 底盘pid和时钟配置

--- Modules/chassis.h
+++ Modules/chassis.h
@@ -59,10 +59,10 @@ typedef struct {
 /* 四个轮子的实车配置，顺序必须和 ChassisWheelIndex 保持一致。 */
 #define CHASSIS_MOTOR_CONFIG_INIT                       \
     {                                                   \
-        {1U,  1, 6.0f, 1.0f, 0.0f}, /* RF 右前 */      \
-        {2U, -1, 6.0f, 1.0f, 0.0f}, /* LF 左前 */      \
-        {3U, -1, 6.0f, 1.0f, 0.0f}, /* LB 左后 */      \
-        {4U,  1, 6.0f, 1.0f, 0.0f}, /* RB 右后 */      \
+        {1U,  1, 10.0f, 80.0f, 0.03f}, /* RF 右前 */      \
+        {2U, -1, 10.0f, 80.0f, 0.03f}, /* LF 左前 */      \
+        {3U, -1, 10.0f, 80.0f, 0.03f}, /* LB 左后 */      \
+        {4U,  1, 10.0f, 80.0f, 0.03f}, /* RB 右后 */      \
     }
 
 void Chassis_Init(void);
--- RobStrideSPC/MDK-ARM/RobStrideEPC/startup_stm32f105xc.d
+++
@@ -1,1 +0,0 @@
-robstrideepc\startup_stm32f105xc.o: startup_stm32f105xc.s
